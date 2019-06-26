c2a152862ed24ec7f71f5baca105cca0dc658492
fayster <gayver91@list.ru>
2019-06-26 23:13:31 +0300

    Test refactoring

--- src/components/ToggleContent/__tests__/index.tsx
+++ src/components/ToggleContent/__tests__/index.tsx
@@ -1,25 +1,30 @@
 import * as React from 'react';
-import { render, fireEvent, getByTestId, findByTestId, findByText, queryByTestId } from '@testing-library/react';
+import { render, fireEvent, getByTestId, queryByTestId } from '@testing-library/react';
 import ToggleContent from '../index';
-import 'jest-dom/extend-expect'
 
-const toggle = (toggle) => <button data-testid="buttonOnShow" onClick={toggle}>Show</button>;
-const content = (hide) => <button data-testid="buttonOnHide" onClick={hide}>Hide</button>;
+const buttonShowDataTestId = 'buttonOnShow';
+const buttonHideDataTestId = 'buttonOnHide';
 
-test('toggle content', async () => {
+const toggle = (show: () => void) =>
+	<button data-testid={ buttonShowDataTestId } onClick={ show }>Show</button>;
+
+const content = (hide: () => void) =>
+	<button data-testid={ buttonHideDataTestId } onClick={ hide }>Hide</button>;
+
+test('Hide and show buttons work', async () => {
 	const { container } = render(<ToggleContent toggle={ toggle } content={ content } />);
+	const showButton = getByTestId(container, buttonShowDataTestId);
 
-	const showButton = getByTestId(container, 'buttonOnShow');
-
-	expect(queryByTestId(container, 'buttonOnHide')).toBeNull();
+	expect(queryByTestId(container, buttonHideDataTestId)).toBeNull();
 
 	fireEvent.click(showButton);
 
-	const hideButton = getByTestId(container, 'buttonOnHide');
+	const hideButton = getByTestId(container, buttonHideDataTestId);
 
 	expect(hideButton).toBeDefined();
+	expect(hideButton.textContent).toEqual('Hide');
 
 	fireEvent.click(hideButton);
 
-	expect(queryByTestId(container, 'buttonOnHide')).toBeNull()
+	expect(queryByTestId(container, buttonHideDataTestId)).toBeNull()
 });
